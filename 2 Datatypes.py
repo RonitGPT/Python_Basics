a47e2e06de945059d02e7b96474605d760fcfb6c
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-08 00:24:39 +0530

    Update 2 Datatypes.py

--- 2 Datatypes.py
+++ 2 Datatypes.py
@@ -9,3 +9,39 @@ LONG :- Large Numbers eg- 9999999999 , 12345609876 .
 BOOLEAN :- True or False
 '''
 # PYTHON IS DYNAMIC TYPE LANGUAGE 
+
+Name = "Ram"
+Roll_no = 42
+Age = 21
+Address = "Navi Mumbai"
+Marks = 456
+Mob_no = 9137997402
+Status = True
+
+print( "My Name is" + " "+Name)
+print("My Roll no is" , Roll_no)
+print("My Age is", Age)
+print("My Address is", Address)
+print("My Marks are",Marks)
+print('My Mobile no is', Mob_no)
+print("My Status is", Status)
+
+#-----------------------------------------------------------------------------------
+
+#USER INPUT PROGRAM
+
+print("***********WELCOME TO MY CHAT BOX************")
+print("What is Your Name",)
+Name=input()
+print("Hello !! Nice to Have you"+" "+Name)
+print("How are You",Name)
+Ans=input()
+print("Glad to hear that your day is", Ans)
+print("what is your Age")
+Age=input()
+print("which city do you belong to?")
+City =input()
+print("Ohh great !! Which part of",City ,"Do you belong to?")
+City1=input()
+
+#------------------------------------------------------------------------------------
