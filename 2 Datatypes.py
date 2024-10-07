f48506dbf681cb5809338116500c365ab187df81
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-07 23:54:00 +0530

    Create 2 Datatypes.py

---
+++ 2 Datatypes.py
@@ -0,0 +1,11 @@
+# BASIC DATATYPES -------
+'''
+VARIABLE :- Name of memory location where we are storing data.
+DATATYPE :- It defines what type of data we are using.
+INTEGER :- Numericals eg- 24,78,735.
+STRING :- Characters with alphabets eg- "ronit" , 'Paris'.
+FLOAT :- Decimal values eg-90.02 , 432.77 . 3.14.
+LONG :- Large Numbers eg- 9999999999 , 12345609876 .
+BOOLEAN :- True or False
+'''
+# PYTHON IS DYNAMIC TYPE LANGUAGE 
