ad6ba85b3e3ae9c239b0057124d3548ade7f6867
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-08 01:06:54 +0530

    Create 3 Arithmatic Operations.py
    addition , cube , percentage , area of circle , area of rectangle

---
+++ 3 Arithmatic Operations.py
@@ -0,0 +1,60 @@
+#ARITHMATIC OPERATIONS
+
+print("Enter your Number A")
+A=int(input())
+print("Enter your Number B")
+B=int(input())
+SUM = A + B
+print("Your Sum =",SUM)
+
+#-----------------------------------------------------------------------------------
+
+#CUBE OF GIVEN NUMBER
+
+print("Enter your Number 'X'")
+X=int(input())
+CUBE= X*X*X
+print("The Cube of",X,"is",CUBE)
+
+#-----------------------------------------------------------------------------------
+
+#ACCPET INPUT OF 5 SUBJECT FROM USER AND CALCULATE ITS TOTAL AND PERCENTAGE
+
+print("Enter Marks of Subject 1")
+a=int(input())
+print("Enter Marks of Subject 2")
+b=int(input())
+print("Enter Marks of Subject 3")
+c=int(input())
+print("Enter Marks of Subject 4")
+d=int(input())
+print("Enter Marks of Subject 5")
+e=int(input())
+Total=a+b+c+d+e
+print("Your Total is",Total)
+Percentage= Total/5
+print("Your Percentage is" , Percentage)
+
+#-----------------------------------------------------------------------------------
+
+#CALCULATE AREA OF CIRCLE
+
+print("Enter the Radius 'r' of the Circle")
+r=int(input())
+pi=3.14
+Area= pi*r*r
+print("Your Area of Circle is",Area)
+
+#-----------------------------------------------------------------------------------
+
+#CALCULATE AREA OF RECTANGLE
+
+print("Enter your Length 'l' ")
+l=float(input())
+print("Enter your Width 'w' ")
+w=float(input())
+a= l*w
+print("Area of Rectangle is",a)
+
+
+
