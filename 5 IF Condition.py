e23cc7214323651f2707ede00431c15be1364565
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-09 00:22:03 +0530

    Create 5 IF Condition.py

---
+++ 5 IF Condition.py
@@ -0,0 +1,36 @@
+ # PROGRAM TO FIND IF THE GIVEN NUMBER IS POSITIVE
+
+a=int(input("Enter your Number = "))
+if (a>=0) :
+    print("Your number is Positive")
+
+#----------------------------------------------------------------
+
+ # PROGRAM TO FIND IF THE GIVEN NUMBER IS POSITIVE OR NEGATIVE
+
+print("Enter Your Number")
+a=int(input())
+if (a>=0) :
+    print("The number",a,"is a POSITIVE number")
+else :
+    print("The number",a,"is a NEGATIVE number")
+
+#-----------------------------------------------------------------
+
+ # PROGRAM TO FIND PASSWORD IS VALID OR NOT
+
+password=input("Enter Your Password = ")
+if (password=="vishal491") :
+    print("Login Successfull")
+else :
+    print("Password is invalid !!")
+    
+#------------------------------------------------------------------
+
+ # PROGRAM TO FIND THE NO IS ODD OR EVEN
+
+x=int(input("Enter your number = "))
+if (x%2==0):
+      print("The number",x,"is a Positive number")
+else :
+    print("The number",x,"is a Negative number")
