e36f90e28435257540c086f6ab31fcf82d7d8fb2
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-09 00:31:52 +0530

    Create 6 IF ELIF Condition.py

---
+++ 6 IF ELIF Condition.py
@@ -0,0 +1,15 @@
+ # PROGRAM TO FIND GREATER BETWEEEN 3 NUMBERS
+
+a=int(input("Enter your number A = "))
+b=int(input("Enter your number B = "))
+c=int(input("Enter your number C = "))
+
+if  ((a>b) and (a>c)):
+    print("A is greater")
+elif ((b>a) and (b>c)):
+    print("B is greater")
+else :
+    print("C is greater")
+    
+
+    
