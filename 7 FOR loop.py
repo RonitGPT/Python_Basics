9498820b150c4fe91e49cec24b7f0bcd3e2cccfb
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-09 19:47:39 +0530

    Create 7 FOR loop.py

---
+++ 7 FOR loop.py
@@ -0,0 +1,23 @@
+ # FOR LOOP
+#=============================================================================
+
+ # Program to display numbers from 1 to 10
+
+for i in range(1,11):
+    print(i)
+
+#-----------------------------------------------------------------------------
+
+ # Program to display numbers upto n numbers
+
+n=int(input("Enter the number n = "))
+for i in range(1,n+1):
+    print(i)
+
+#-----------------------------------------------------------------------------
+
+ # Program to display table of n number
+
+n=int(input("Enter you number = "))
+for x in range(1,11):
+    print(n,"*",x,"=",x*n)
