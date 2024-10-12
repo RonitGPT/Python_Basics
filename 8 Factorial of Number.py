cdcecc03a7e45671b06b86ec50f35b9143e20fb6
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-12 22:08:23 +0530

    Create 8 Factorial of Number.py

---
+++ 8 Factorial of Number.py
@@ -0,0 +1,8 @@
+    # PROGRAM TO FIND THE FACTORIALOF NUMBER 
+
+x = int(input("ENTER YOUR NO :- "))
+Fact = 1 
+for i in range (1 , x + 1): 
+    Fact = Fact * i 
+    i = i + 1 
+print (" Factorial of number is = ", Fact)
