02ece7f1322a70eae9d914cc72a32fb1ea50f04e
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-15 23:21:09 +0530

    Create 11 WHILE loop.py

---
+++ 11 WHILE loop.py
@@ -0,0 +1,28 @@
+'''
+WHILE LOOP ------> It is used when we don,t know the end of iterations
+this is called as entry control loop it means when we are entering into loop conditions will be checkes
+
+SYNTAX :
+WHILE (condition):
+statement
+incr/decre
+'''
+
+ # TO PRINT 1 TO N NUMBERS USING WHILE LOOP
+
+n=int(input("Enter value of n = "))
+i=1
+while (i<=n):
+    print(i)
+    i=i+1
+
+#---------------------------------------------------------------------------------------------------
+
+ # TO PRINT 10 TO N NUMBERS USING WHILE LOOP
+
+n=int(input("Enter value of n = "))
+i=10
+while (i>=n):
+    print(i)
+    i=i-1
+ 
