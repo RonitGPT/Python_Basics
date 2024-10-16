4432b15f2b0392bb6a002d3d0b65a4b533be5e79
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-16 23:07:21 +0530

    Create 15 Branching statement.py

---
+++ 15 Branching statement.py
@@ -0,0 +1,16 @@
+    # BRANCHING STATEMENTS :- BREAK / CONTINUE
+
+# BREAK 
+
+for i in range (1,11):
+    if (i==6):
+        break
+    print(i)
+#--------------------------------------------------------------------------------------------------
+
+# CONTINUE
+
+for i in range (1,11):
+    if (i==6):
+        continue
+    print(i)
