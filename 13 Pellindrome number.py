658f47e8da56feeda8fc7c9b3450047a4c33c06e
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-16 23:07:13 +0530

    Create 13 Pellindrome number.py

---
+++ 13 Pellindrome number.py
@@ -0,0 +1,13 @@
+    # PROGRAM TO FIND IF THE NUMBER IS PELLINDROME
+
+x = int(input("Enter your number x = "))
+n=x
+sum=0
+while (x>0):
+    y = x % 10
+    sum = sum*10 + y
+    x = x // 10
+if (sum == n):
+    print("Number is PELLINDROME")
+else:
+    print("Number is NOT PELLINDROME")
