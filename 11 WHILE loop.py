ab8ccde6c4efcb827c0a3b7c3748c466c7081479
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-16 23:07:06 +0530

    Update 11 WHILE loop.py

--- 11 WHILE loop.py
+++ 11 WHILE loop.py
@@ -1,28 +1,31 @@
 '''
-WHILE LOOP ------> It is used when we don,t know the end of iterations
-this is called as entry control loop it means when we are entering into loop conditions will be checkes
+WHILE LOOP :- It is used when we don't know the number of iterations 
+It is known as enerty control loop it means when we are entering into loop ,  condition will be checked
 
-SYNTAX :
-WHILE (condition):
-statement
-incr/decre
+SYNTAX :-
+WHILE (CONDITION):
+    STATEMENT
+    INCREMENT / DECREMENT
+
 '''
 
- # TO PRINT 1 TO N NUMBERS USING WHILE LOOP
-
-n=int(input("Enter value of n = "))
+    # PROGRAM TO PRINT 1 TO N NUMBERS
+'''
+n = int(input("Enter your number n :- "))
 i=1
 while (i<=n):
+    print (i)
+    i += 1
+'''
+#---------------------------------------------------------------------------------------------------------------
+
+    # ROGRAM TO PRINT N TO 1 NUMBERS
+'''
+n = int(input("Enter your number n :- "))
+i=n
+while (i>0):
     print(i)
-    i=i+1
+    i -= 1
+'''
+#----------------------------------------------------------------------------------------------------------------
 
-#---------------------------------------------------------------------------------------------------
-
- # TO PRINT 10 TO N NUMBERS USING WHILE LOOP
-
-n=int(input("Enter value of n = "))
-i=10
-while (i>=n):
-    print(i)
-    i=i-1
- 
