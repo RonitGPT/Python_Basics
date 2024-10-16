fb1016e33a136de4f9467faa9baab1202ed578fd
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-16 23:07:17 +0530

    Create 14 Armstrong number.py

---
+++ 14 Armstrong number.py
@@ -0,0 +1,16 @@
+# PROGRAM TO FIND IF THE NUMBER IS PELLINDROME OR NOT (ONLY FOR 3 DIGITS)
+
+x = int (input("Enter the numbe 'x :- " ))
+n = x
+sum = 0
+while (x>0):
+    y = x % 10
+    a = y*y*y
+    sum = sum + a
+    x = x // 10
+if (sum == n):
+    print ("Number is ARMSTRONG")
+else:
+    print ("Number is NOT ARMSTRONG")
+
+    
