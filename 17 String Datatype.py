fc6b194cdc9a7f40d3c54955943dc60a6736deb3
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-18 01:36:30 +0530

    Update 17 String Datatype.py

--- 17 String Datatype.py
+++ 17 String Datatype.py
@@ -1,35 +1,39 @@
+#==========================================================================================================================
+        # MORE DATA TYPES ON PYTHON :- STRING , LIST , TUPLE , DICTIONARY , SET.
+#==========================================================================================================================
+
+    # STRING DATATYPE
+
 '''
-More Data types in python ----> String , List , Tuple , disctionary 
-1. String data types are ----> Sequece of character placed in ''," ", ''' '''
-inbuild function in string
+STRING DATATYPE :- Sequence of characters placed in ' ', " " , """ """ .
+Inbuild function string
 '''
 
-str = "itvedant Thane" 
-str2 = "hello"
-list1 = ['1','2','3','4','5']
-print (str.capitalize())         # Display first alpha in capital
-print(str.upper())
-print(str.lower())
+str1 = "itvedant thane"
+str2 = " hello "
 
-print(str.title())             # Display every word's first alpha in capital
+#------------------------------------------------------------------------------------------------------------------------
 
-print(str.isupper())
-print(str.islower())
+print(str1.capitalize())      # Display first letter in Capital
+print(str1.upper())
+print(str1.lower())
 
-print(len(str))
+print(str1.title())           # Display every word's first letter in capital
 
-print(str.swapcase())
+print(str1.isupper())
+print(str1.islower())
 
-print(str.isalpha())
-print(str2.islower())
+print(len(str1))
 
-result= str.join(list1)
+print(str1.swapcase())
+
+print(str1.isalpha())
+print(str1.islower())
+
+result = str1.join(str2)
 print(result)
 
-print(min(str))
-print(max(str))
+print(min(str1))
+print(max(str1))
 
-print(str.replace('Thane','Mumbai'))
-
-
-
+print(str1.replace("thane", "Mumbai"))
