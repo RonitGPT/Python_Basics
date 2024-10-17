3a94ab40b54fba68d95a2cb4dbdbd234ae1419ff
RonitGPT <ronitsuryarao4750@gmail.com>
2024-10-18 01:36:45 +0530

    Update 18 List Datatype.py

--- 18 List Datatype.py
+++ 18 List Datatype.py
@@ -1,44 +1,44 @@
+#===================================================================================================================
+                                    # LIST DATATYPE
+#===================================================================================================================
+
 '''
-LIST  data type ---->  It is Mutable Data Type i.e ( CHANGEABLE Datatype)
-Lists are represented by []
-List are collection of different types of data
+LIST DATATYPE -----> IT IS MUTABLE DATATYPE (CHANGABLE DATATYPE)
+LIST are collection of different types of data
+They are represented by []
 '''
-'''
-list1 = [ 1, "Vishal", 25 , 96.75 , "M" , "M"]
-list2 = [ "JAVA","SQL" , "PYTHON"]
+#------------------------------------------------------------------------------------------------------------------
 
-print (len(list1))
-s= list2.reverse(list1)
-print(s) 
+List1 = [ "Ronit", "Manasi", "Vishal", "Yash", "Niharika", "Pradnya", "Chaitali","Sushant", "Aakash" ]
 
-k = list1.count("M")
-print(k)
+#------------------------------------------------------------------------------------------------------------------
 
-list2.append("DJANGO")
-print(list2)
+# To print element of list
+print(List1)
 
-print(min(list2))
-print(max(list2))
-'''
+# To print lenght of list
+print(len(List1))
 
-# ------------------------------------------------------------------------------------------------------
-list1 = ['vishal', 'yash','ronit', 'manasi','akash','nitin']
+# To print Maximum string in list
+print (max(List1))
 
-print("element in list - " , list1)
-print("length of list -" , len(list1))
-print("Maximum str in list - ", max(list1))
-print("Minimum str in list - ", min(list1))
-print("First element in list - ", list1[0])
-print("Slicing in list - ", list1[1:4])
-list1.append("sushant")
-print("APPENDED LIST IS - ", list1)
-list1.pop(1)
-print("Removed element is - ", list1)
-list1.reverse()
-print("Reversed element is - ", list1)
+# To print Minimun string in list
+print(min(List1))
 
+# To print first element in list
+print(List1[0])
 
+# Slicing in list
+print(List1[1:4])
 
+# To add item in list
+List1.append("Nitin")
+print(List1)
 
+# To remove item from list
+List1.pop(7)
+print(List1)
 
-
+# To reverse item in List
+List1.reverse()
+print(List1)
