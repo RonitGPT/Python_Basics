efc9acf8be532d17dddb4c600f2eff0630a7d6cf
RonitGPT <ronitsuryarao4750@gmail.com>
2024-11-05 23:42:58 +0530

    Update 24 Mini-Project (Crude Operation).py

--- 24 Mini-Project (Crude Operation).py
+++ 24 Mini-Project (Crude Operation).py
@@ -1,47 +1,61 @@
 #=====================================================================================================
                                         # CRUDE OPERATIONS
 #=====================================================================================================
-
 List = []
+#------------------------------------------------------------------------------------------------------
+
+# Functions 
 
 def add_item():
-    print("Add Item")
+    name = input("Enter name of the Item :- ")
+    price = float(input("Price of the Item :- "))
+    new_item = (name, price)
+    List.append(new_item)
+    print("The item '{}' with price '{}' was succesfully added ".format(name, price))
 
 def show_item():
-    print("Show Item")
+    if not List:
+        print("Your List is empty")
+    else :
+        print("Your List contains :- ")
+    for i,X in enumerate (List , 0):
+    
+        print("{}. {}-{} ".format(i. X['name'],X['price']))
+    '''
+    print ("\n" , List(name, price))
+    '''
 
 def delete_item():
-    print("Delete Item")
+    pass
 
 def update_item():
-    print("Update item")
+    pass
 
-while True :
-    print("*** MENU ***")
-    print("1. Insert Item ")
-    print("2. Show Item ")
-    print("3. Delete Item ")
-    print("4. Update Item ")
-    print("5. Exit ")
 
-    choose = input("Enter Number to perform Operation :- ")
+while True:
+    print("***** MENU *****")
+    print("1. ADD ITEMS")
+    print("2. SHOW ITEMS")
+    print("3. REMOVE ITEMS")
+    print("4. UPDATE ITEMS")
+    print("5. EXIT ")
 
-    if (choose == '1'):
+    choise = input("Enter the Operation Number :- ")
+
+    if (choise == "1"):
         add_item()
-    elif (choose == '2'):
+    elif (choise == "2"):
         show_item()
-    elif (choose == '3'):
+    elif (choise == "3"):
         delete_item()
-    elif (choose == '4'):
+    elif (choise == "4"):
         update_item()
-    elif (choose == '5'):
+    elif (choise == "5"):
+        print("Exiting")
         break
     else :
-        print ("Invalid Option")
-
-    ans = input("Do you want to Continue.. Press 'yes' :- ")
-    if (ans.lower()!= "yes" ):
+        print("Invalid Choice")
+    
+    ans = input("If you want to continuee.. enter 'y' :- ")
+    if (ans.lower()!= "y"):
         break
-    
-    
-    
