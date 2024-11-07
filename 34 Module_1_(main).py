749bbc73fa08864f31443901e63c25e777cf20b9
RonitGPT <ronitsuryarao4750@gmail.com>
2024-11-07 21:04:03 +0530

    Create 34 Module_1_(main).py

---
+++ 34 Module_1_(main).py
@@ -0,0 +1,17 @@
+#===================================================================================================================================
+                                                #  MODULE
+#===================================================================================================================================
+'''
+IT IS FILE THAT CONTAILS THE FUNCTIONS 
+To call the module :-
+                import modulename       OR
+                from modulename import function
+'''
+
+# Functions
+
+def Add(x,y):
+    return x + y
+
+def Sub(x,y):
+    return x - y
