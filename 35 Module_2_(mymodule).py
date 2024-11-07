10bd86ec241f53e764746f16ff0da8b072a3b1f5
RonitGPT <ronitsuryarao4750@gmail.com>
2024-11-07 21:04:09 +0530

    Create 35 Module_2_(mymodule).py

---
+++ 35 Module_2_(mymodule).py
@@ -0,0 +1,8 @@
+    # IN THIS FILE WE CAN DIRECTLY CALL MAIN MODULE FILE TO ACCESS FUNCTIONS 
+'''
+# import 34 Module_1_(main) as m   ----> as program name is not valid it will show error (Give simple file name to import)
+
+print ("Addition is ", m.Add(7,8))
+print ("Subtraction is ",m.Sub(8,12))
+
+'''   
